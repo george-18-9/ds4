06ed5a486e70079c9fe986aa995f1108f6414795
George <george-18-9>
2025-09-22 19:50:32 -0500

    Laboratorio3

---
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EjemploLectura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            string texto = Console.ReadLine();  // Lee lo que escriba el usuario
+            int numero = Convert.ToInt32(texto); // Convierte el texto a entero
+
+            Console.WriteLine("El número ingresado es: " + numero);
+        }
+    }
+}
+
