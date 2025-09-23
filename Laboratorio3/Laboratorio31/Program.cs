77bf824772090412ade965083c89b5f7af5b7f78
George <george-18-9>
2025-09-22 19:49:02 -0500

    Laboratorio31

---
+++ Laboratorio3/Laboratorio31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Laboratorio31
+{
+    class CalculosMatematicos
+    {
+        public static int Calcular(int a, int b)
+        {
+            return (a + b) * (a - b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer número: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = CalculosMatematicos.Calcular(a, b);
+            Console.WriteLine($"El resultado de (a+b)*(a-b) es: {resultado}");
+        }
+    }
+}
+
