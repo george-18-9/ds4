360a0caac35a3220882d8bfd7491b2d8573611ca
George <george-18-9>
2025-09-22 19:17:57 -0500

    laboratorio21

---
+++ Laboratorio2/Laboratorio21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+     public class Program
+    {
+        public static void Main()
+        {
+            //asignando valor a variable estatica
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        //Declarando Variable estatica
+        public static int Valor;
+    }
+}
