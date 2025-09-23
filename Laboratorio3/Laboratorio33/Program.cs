7e2cf8803b6fe4919b7c9cc0ad30e813bb84ca2f
George <george-18-9>
2025-09-22 19:49:39 -0500

    Laboratorio33

---
+++ Laboratorio3/Laboratorio33/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Laboratorio33
+{
+    class CalculosMatematicos
+    {
+        public static double CalculoPerimetro(double lado1, double lado2)
+        {
+            return 2 * (lado1 + lado2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor del primer lado: ");
+            double lado1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el valor del segundo lado: ");
+            double lado2 = Convert.ToDouble(Console.ReadLine());
+
+            double perimetro = CalculosMatematicos.CalculoPerimetro(lado1, lado2);
+            Console.WriteLine($"El perímetro del rectángulo es: {perimetro}");
+        }
+    }
+}
