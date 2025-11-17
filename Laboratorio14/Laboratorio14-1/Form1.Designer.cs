98f354ad23a8a9dc3f090bb760371a0fadf77d07
George <georgeworrell9@gmail.com>
2025-11-17 18:41:00 -0500

    terminado

---
+++ Laboratorio14/Laboratorio14-1/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Laboratorio14_1
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProductos));
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tstId = new System.Windows.Forms.ToolStripTextBox();
+            this.tsbBuscar = new System.Windows.Forms.ToolStripButton();
+            this.tsbNuevo = new System.Windows.Forms.ToolStripButton();
+            this.tsbGuardar = new System.Windows.Forms.ToolStripButton();
+            this.tsbCancelar = new System.Windows.Forms.ToolStripButton();
+            this.tsbEliminar = new System.Windows.Forms.ToolStripButton();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(51, 102);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(118, 26);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(240, 102);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(344, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(51, 210);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(121, 26);
+            this.txtPrecio.TabIndex = 2;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(244, 210);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(145, 26);
+            this.txtStock.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tstId,
+            this.tsbBuscar,
+            this.tsbNuevo,
+            this.tsbGuardar,
+            this.tsbCancelar,
+            this.tsbEliminar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 33);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tstId
+            // 
+            this.tstId.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.tstId.Name = "tstId";
+            this.tstId.Size = new System.Drawing.Size(100, 33);
+            // 
+            // tsbBuscar
+            // 
+            this.tsbBuscar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbBuscar.Image = ((System.Drawing.Image)(resources.GetObject("tsbBuscar.Image")));
+            this.tsbBuscar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBuscar.Name = "tsbBuscar";
+            this.tsbBuscar.Size = new System.Drawing.Size(34, 28);
+            this.tsbBuscar.Text = "Buscar";
+            this.tsbBuscar.Click += new System.EventHandler(this.tsbBuscar_Click);
+            // 
+            // tsbNuevo
+            // 
+            this.tsbNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNuevo.Image")));
+            this.tsbNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNuevo.Name = "tsbNuevo";
+            this.tsbNuevo.Size = new System.Drawing.Size(34, 28);
+            this.tsbNuevo.Text = "Nuevo";
+            this.tsbNuevo.Click += new System.EventHandler(this.tsbNuevo_Click);
+            // 
+            // tsbGuardar
+            // 
+            this.tsbGuardar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbGuardar.Image = ((System.Drawing.Image)(resources.GetObject("tsbGuardar.Image")));
+            this.tsbGuardar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbGuardar.Name = "tsbGuardar";
+            this.tsbGuardar.Size = new System.Drawing.Size(34, 28);
+            this.tsbGuardar.Text = "Guardar";
+            this.tsbGuardar.Click += new System.EventHandler(this.tsbGuardar_Click);
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tsbCancelar.Image")));
+            this.tsbCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Size = new System.Drawing.Size(34, 28);
+            this.tsbCancelar.Text = "Cancelar";
+            this.tsbCancelar.Click += new System.EventHandler(this.tsbCancelar_Click);
+            // 
+            // tsbEliminar
+            // 
+            this.tsbEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbEliminar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEliminar.Image")));
+            this.tsbEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEliminar.Name = "tsbEliminar";
+            this.tsbEliminar.Size = new System.Drawing.Size(34, 28);
+            this.tsbEliminar.Text = "Eliminar";
+            this.tsbEliminar.Click += new System.EventHandler(this.tsbEliminar_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(59, 61);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(41, 29);
+            this.lblId.TabIndex = 6;
+            this.lblId.Text = "Id";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(240, 61);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(116, 29);
+            this.lblNombre.TabIndex = 7;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(59, 176);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(104, 29);
+            this.lblPrecio.TabIndex = 8;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStock.Location = new System.Drawing.Point(240, 176);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(92, 29);
+            this.lblStock.TabIndex = 9;
+            this.lblStock.Text = "Stock";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(62, 286);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(152, 42);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Name = "frmProductos";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmProductos_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox tstId;
+        private System.Windows.Forms.ToolStripButton tsbBuscar;
+        private System.Windows.Forms.ToolStripButton tsbNuevo;
+        private System.Windows.Forms.ToolStripButton tsbGuardar;
+        private System.Windows.Forms.ToolStripButton tsbCancelar;
+        private System.Windows.Forms.ToolStripButton tsbEliminar;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Laboratorio14/Laboratorio14-1/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Laboratorio14_1
+{
+    public partial class frmProductos : Form
+    {
+        string connectionString = @"Server=.\SQLEXPRESS;Database=productos;Trusted_Connection=True;";
+        bool nuevo = false;
+
+        private void LimpiarCampos()
+        {
+            txtId.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtStock.Text = "";
+        }
+
+        private void HabilitarCampos(bool estado)
+        {
+            // si estado = true -> habilita para edición/entrada
+            txtNombre.Enabled = estado;
+            txtPrecio.Enabled = estado;
+            txtStock.Enabled = estado;
+            // Id normalmente readonly
+            txtId.ReadOnly = true;
+            tsbGuardar.Enabled = estado;
+            tsbCancelar.Enabled = estado;
+            tsbNuevo.Enabled = !estado;
+            tsbEliminar.Enabled = !estado;
+        }
+
+        private void CargarTodos()
+        {
+            // Opcional: carga al DataGridView
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand("SELECT Id, Nombre, Precio, Stock FROM dbo.Laptops", cn))
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (this.Controls.Find("dgvProductos", true).Length > 0)
+                    {
+                        var dgv = (DataGridView)this.Controls["dgvProductos"];
+                        dgv.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar productos: " + ex.Message);
+            }
+        }
+
+
+        public frmProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void tsbBuscar_Click(object sender, EventArgs e)
+        {
+            // obtener valor desde ToolStripTextBox
+            string sId = tstId.Text.Trim();
+            int id;
+            if (!int.TryParse(sId, out id))
+            {
+                MessageBox.Show("Ingrese un Id válido en la caja de búsqueda.");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand("SELECT Id, Nombre, Precio, Stock FROM dbo.Laptops WHERE Id = @id", cn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cn.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            txtId.Text = dr["Id"].ToString();
+                            txtNombre.Text = dr["Nombre"].ToString();
+                            txtPrecio.Text = dr["Precio"].ToString();
+                            txtStock.Text = dr["Stock"].ToString();
+
+                            // Habilitar edición si desea permitir editar directamente
+                            nuevo = false;
+                            HabilitarCampos(true); // si quieres editar inmediatamente
+                        }
+                        else
+                        {
+                            MessageBox.Show("Registro no encontrado.");
+                        }
+                    }
+                    cn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en búsqueda: " + ex.Message);
+            }
+        }
+
+
+        private void tsbNuevo_Click(object sender, EventArgs e)
+        {
+            nuevo = true;
+            LimpiarCampos();
+            HabilitarCampos(true);
+            txtNombre.Focus();
+        }
+
+
+        private void tsbGuardar_Click(object sender, EventArgs e)
+        {
+            // Validaciones básicas
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("El nombre es obligatorio.");
+                txtNombre.Focus();
+                return;
+            }
+
+            decimal precio;
+            int stock;
+            if (!decimal.TryParse(txtPrecio.Text, out precio))
+            {
+                MessageBox.Show("Precio inválido.");
+                txtPrecio.Focus();
+                return;
+            }
+            if (!int.TryParse(txtStock.Text, out stock))
+            {
+                MessageBox.Show("Stock inválido.");
+                txtStock.Focus();
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    cn.Open();
+
+                    if (nuevo)
+                    {
+                        string insertSql = "INSERT INTO dbo.Laptops (Nombre, Precio, Stock) VALUES (@nombre, @precio, @stock);";
+                        using (SqlCommand cmd = new SqlCommand(insertSql, cn))
+                        {
+                            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                            cmd.Parameters.AddWithValue("@precio", precio);
+                            cmd.Parameters.AddWithValue("@stock", stock);
+                            cmd.ExecuteNonQuery();
+                        }
+                        MessageBox.Show("Registro guardado (insert).");
+                    }
+                    else
+                    {
+                        // editar
+                        int id;
+                        if (!int.TryParse(txtId.Text, out id))
+                        {
+                            MessageBox.Show("Id inválido.");
+                            return;
+                        }
+
+                        string updateSql = "UPDATE dbo.Laptops SET Nombre=@nombre, Precio=@precio, Stock=@stock WHERE Id=@id;";
+                        using (SqlCommand cmd = new SqlCommand(updateSql, cn))
+                        {
+                            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                            cmd.Parameters.AddWithValue("@precio", precio);
+                            cmd.Parameters.AddWithValue("@stock", stock);
+                            cmd.Parameters.AddWithValue("@id", id);
+                            int afect = cmd.ExecuteNonQuery();
+                            if (afect > 0)
+                                MessageBox.Show("Registro actualizado.");
+                            else
+                                MessageBox.Show("No se encontró el registro para actualizar.");
+                        }
+                    }
+
+                    cn.Close();
+                }
+
+                // después de guardar
+                nuevo = false;
+                HabilitarCampos(false);
+                LimpiarCampos();
+                CargarTodos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar: " + ex.Message);
+            }
+        }
+
+
+        private void tsbCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbEliminar_Click(object sender, EventArgs e)
+        {
+            int id;
+            if (!int.TryParse(txtId.Text, out id))
+            {
+                MessageBox.Show("Ingrese un Id válido para eliminar.");
+                return;
+            }
+
+            var r = MessageBox.Show("¿Seguro que desea eliminar el registro con Id " + id + "?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (r != DialogResult.Yes) return;
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM dbo.Laptops WHERE Id = @id", cn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cn.Open();
+                    int afect = cmd.ExecuteNonQuery();
+                    cn.Close();
+
+                    if (afect > 0)
+                    {
+                        MessageBox.Show("Registro eliminado.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontró registro con ese Id.");
+                    }
+                    LimpiarCampos();
+                    CargarTodos();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar: " + ex.Message);
+            }
+        }
+
+
+        private void frmProductos_Load(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            HabilitarCampos(false);
+            CargarTodos();
+        }
+
+    }
+}
